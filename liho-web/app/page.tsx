1d7df274d3145da40ea73db0ad9f7b565dc0c4a9
William Wang <william1997au@gmail.com>
2025-09-04 21:44:05 +1000

    chore(web): move liho-web to separate repo (LiHo-Chat-Web)

--- liho-web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>LiHo Web</h1>
-      <p>Next.js + TypeScript skeleton. Visit /health to verify API.</p>
-    </main>
-  );
-}
-
